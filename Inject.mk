83432e08be186f7f16e527aa4cb4fb01454bfaf8
nathan <zyc325129@gmail.com>
2019-05-12 20:48:11 +0800

    整理分支文件

--- Inject.mk
+++
@@ -1,8 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-    LOCAL_MODULE := inject
-    LOCAL_SRC_FILES := inject.c	tools.c
-    LOCAL_LDLIBS += -L$(SYSROOT)/usr/lib -llog
-include $(BUILD_EXECUTABLE)
-
